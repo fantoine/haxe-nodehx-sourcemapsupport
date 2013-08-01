0f55dd2d55791408f76f93a453ceec02842f425a
fab.antoine@gmail.com <fab.antoine@gmail.com>
2013-08-01 17:06:58 +0200

    first commit

---
+++ nodehx/sourcemapsupport/SourceMapSupportModule.hx
@@ -0,0 +1,5 @@
+package nodehx.sourcemapsupport;
+
+extern class SourceMapSupportModule implements nodehx.Node.NodeModule<"source-map-support", "", ""> {
+	public function install(?options : Dynamic) : Void;
+}
